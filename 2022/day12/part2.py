a890f4629c4779389ecc33e7de40ecf01aef9e43
Eric <ewetzel@ucsb.edu>
2022-12-12 00:00:44 -0800

    day 12 optimized

--- 2022/day12/part2.py
+++ 2022/day12/part2.py
@@ -21,24 +21,9 @@ def neighbors(coord):
     e = (x+1, y)
     s = (x, y+1)
     w = (x-1, y)
-    return filter(lambda c: c in grid, [n,e,s,w])
+    return filter(lambda c: c in grid and grid[(x, y)] - grid[c] <= 1, [n,e,s,w])
 
-def cleanup_a(c, to_remove):
-    if c in to_remove: return
-    if all(grid[n] == ord('a') for n in neighbors(c)):
-        to_remove.add(c)
-        for n in neighbors(c):
-            cleanup_a(n, to_remove)
-
-to_remove = set()
-for c in grid:
-    if grid[c] == ord('a'):
-        cleanup_a(c, to_remove)
-
-for a in to_remove:
-    del grid[a]
-
-def dijkstra(src, dst):
+def dijkstra(src, dst = None):
     dist = {}
     queue = []
     for node in grid:
@@ -49,11 +34,12 @@ def dijkstra(src, dst):
     heappush(queue, (0, src))
     while len(queue) > 0:
         current = heappop(queue)[1]
-        for neighbor in filter(lambda n: grid[n] - grid[current] <= 1, neighbors(current)):
+        for neighbor in neighbors(current):
             cost = dist[current] + 1
             if cost < dist[neighbor]:
                 dist[neighbor] = cost
                 heappush(queue, (cost, neighbor))
-    return dist[dst]
+    return dist if dst is None else dist[dst]
 
-print(min(dijkstra(c, e_pos) for c in grid if grid[c] == ord('a') and ord('b') in map(grid.get, neighbors(c))))
+dist = dijkstra(e_pos)
+print(min(dist[c] for c in grid if grid[c] == ord('a')))
