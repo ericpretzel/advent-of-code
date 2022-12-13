bedb19bcd22df6600c39f463b958910b001c85ce
Eric <ewetzel@ucsb.edu>
2022-12-12 22:37:30 -0800

    optimized day 13

--- 2022/day13/part1.py
+++ 2022/day13/part1.py
@@ -2,18 +2,13 @@ pi = [line.strip() for line in open('input.in').readlines()]
 
 def compare(a, b):
     if isinstance(a, int) and isinstance(b, int):
-        if a < b:
-            return True
-        elif a == b:
-            return None
-        else:
-            return False
+        return a - b
     elif isinstance(a, list) and isinstance(b, list):
         for i, j in zip(a, b):
             c = compare(i, j)
-            if c is not None:
+            if c != 0:
                 return c
-        return len(a) <= len(b)
+        else: return len(a) - len(b)
     elif isinstance(a, int):
         return compare([a], b)
     else:
@@ -23,7 +18,7 @@ ordered = 0
 p = 1
 for i in range(0, len(pi), 3):
     a, b = map(eval, pi[i:i+2])
-    if compare(a, b):
+    if compare(a, b) < 0:
         ordered += p
     p += 1
 
